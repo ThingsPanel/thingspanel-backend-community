08edc0aa3d14d937d5e571f37cf59fd280a42d45
fengerjinyi <290806912@qq.com>
2021-12-07 22:05:29 +0800

    Delete pkg directory

--- pkg/config/config.go
+++
@@ -1,9 +0,0 @@
-package config
-
-// config package generalizes the application configurations
-
-type Config interface {
-	Print() string
-	Validate() error
-	Init() error
-}
--- pkg/env/env.go
+++
@@ -1,7 +0,0 @@
-package env
-
-import "github.com/caarlos0/env/v6"
-
-func Parse(in interface{}) error {
-	return env.Parse(in)
-}
