1d6de3181377a5a81e84a0503896a00b9a764d46
hezhuozhuo <421045249@qq.com>
2021-12-29 20:15:15 +0800

    logmap

--- middleware/log_middle.go
+++ middleware/log_middle.go
@@ -7,6 +7,7 @@ import (
 	uuid "ThingsPanel-Go/utils"
 	"encoding/json"
 	"fmt"
+	"strings"
 	"time"
 
 	adapter "github.com/beego/beego/v2/adapter"
@@ -40,12 +41,16 @@ var filterLog = func(ctx *context.Context) {
 			name = userClaims.Name
 		}
 	}
+	//获取url类型
+	urlKey := strings.Replace(ctx.Input.URL(), "/", "_", -1)
+	urlType := urlMap(urlKey)
+
 	detailedStruct.Name = name
 	describe := name + "-send:" + detailedStruct.Path
 	var uuid = uuid.GetUuid()
 	logData := models.OperationLog{
 		ID:        uuid,
-		Type:      "1", //需要确认需求
+		Type:      urlType, //需要确认需求
 		Describe:  describe,
 		DataID:    "",
 		CreatedAt: time.Now().Unix(),
@@ -55,6 +60,106 @@ var filterLog = func(ctx *context.Context) {
 		fmt.Println("log insert fail")
 	}
 
+}
+
+func urlMap(k string) string {
+	myColors := map[string]string{
+		"_api_auth_login":                "1",
+		"_api_auth_logout":               "1",
+		"_api_auth_refresh":              "1",
+		"_api_auth_me":                   "1",
+		"_api_home_list":                 "2",
+		"_api_home_chart":                "2",
+		"_api_index_show":                "2",
+		"_api_index_device":              "2",
+		"_api_user_index":                "3",
+		"_api_user_add":                  "3",
+		"_api_user_edit":                 "3",
+		"_api_user_delete":               "3",
+		"_api_user_password":             "3",
+		"_api_user_update":               "3",
+		"_api_user_permission":           "3",
+		"_api_customer_index":            "4",
+		"_api_customer_add":              "4",
+		"_api_customer_edit":             "4",
+		"_api_customer_delete":           "4",
+		"_api_asset_index":               "5",
+		"_api_asset_add":                 "5",
+		"_api_asset_edit":                "5",
+		"_api_asset_delete":              "5",
+		"_api_asset_widget":              "5",
+		"_api_asset_list":                "5",
+		"_apiasset_work_index":           "5",
+		"_apiasset_work_add":             "5",
+		"_apiasset_work_edit":            "5",
+		"_apiasset_work_delete":          "5",
+		"_apibusiness_index":             "5",
+		"_apibusiness_add":               "5",
+		"_apibusiness_edit":              "5",
+		"_apibusiness_delete":            "5",
+		"_apibusiness_tree":              "5",
+		"_api_device_token":              "6",
+		"_api_device_index":              "6",
+		"_api_device_edit":               "6",
+		"_api_device_add":                "6",
+		"_api_device_delete":             "6",
+		"_api_device_configure":          "6",
+		"_api_dashboard_index":           "7",
+		"_api_dashboard_add":             "7",
+		"_api_dashboard_edit":            "7",
+		"_api_dashboard_delete":          "7",
+		"_api_dashboard_paneladd":        "7",
+		"_api_dashboard_paneldelete":     "7",
+		"_api_dashboard_paneledit":       "7",
+		"_api_dashboard_list":            "7",
+		"_api_dashboard_business":        "7",
+		"_api_dashboard_property":        "7",
+		"_api_dashboard_device":          "7",
+		"_api_dashboard_inserttime":      "7",
+		"_api_dashboard_gettime":         "7",
+		"_api_dashboard_dashboard":       "7",
+		"_api_dashboard_realTime":        "7",
+		"_api_dashboard_updateDashboard": "7",
+		"_api_dashboard_component":       "7",
+		"_api_markets_list":              "7",
+		"_api_warning_index":             "8",
+		"_api_warning_list":              "8",
+		"_api_warning_field":             "8",
+		"_api_warning_add":               "8",
+		"_api_warning_edit":              "8",
+		"_api_warning_delete":            "8",
+		"_api_warning_show":              "8",
+		"_api_warning_update":            "8",
+		"_api_automation_index":          "9",
+		"_api_automation_add":            "9",
+		"_api_automation_edit":           "9",
+		"_api_automation_delete":         "9",
+		"_api_automation_get_by_id":      "9",
+		"_api_automation_status":         "9",
+		"_api_automation_symbol":         "9",
+		"_api_automation_property":       "9",
+		"_api_automation_show":           "9",
+		"_api_automation_update":         "9",
+		"_api_automation_instruct":       "9",
+		"_api_operation_index":           "10",
+		"_api_operation_list":            "10",
+		"_api_structure_add":             "10",
+		"_api_structure_list":            "10",
+		"_api_structure_update":          "10",
+		"_api_structure_delete":          "10",
+		"_api_structure_field":           "10",
+		"_api_navigation_add":            "10",
+		"_api_navigation_list":           "10",
+		"_api_kv_list":                   "10",
+		"_api_kv_index":                  "10",
+		"_api_kv_export":                 "10",
+	}
+	_, ok := myColors[k]
+	if ok {
+		return myColors[k]
+	} else {
+		return ""
+	}
 }
 
 //获取请求详细数据
