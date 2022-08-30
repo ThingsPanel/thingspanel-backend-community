938b547e4af887b0526d13dca7fa9ee411b89178
hezhuozhuo <421045249@qq.com>
2022-08-30 16:09:04 +0800

    update

--- controllers/home.go
+++ controllers/home.go
@@ -168,8 +168,8 @@ func (HomeController *HomeController) GetDefaultSetting() {
 				}
 				d["default_setting"] =
 					"MQTT接入点: " + viper.GetString("url") + ":" + port +
-						"$$设备订阅主题: " + viper.GetString("mqtt.topicToPublish") + "/{Token}" +
 						"$$设备发布主题: " + viper.GetString("mqtt.topicToSubscribe") +
+						"$$设备订阅主题: " + viper.GetString("mqtt.topicToPublish") + "/{Token}" +
 						"$$设备自定义发布主题: custom/pub/{Token}/+" +
 						"$$设备自定义订阅主题: custom/sub/{Token}/+" +
 						"$$MQTT用户名: {Token}" +
