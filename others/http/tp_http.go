c3cc69b9d1da1006726849253bd2a5b1aa60e195
hezhuozhuo <421045249@qq.com>
2022-11-02 19:37:30 +0800

    update

--- others/http/tp_http.go
+++ others/http/tp_http.go
@@ -11,16 +11,28 @@ import (
 )
 
 func Post(targetUrl string, payload string) (*http.Response, error) {
+	logs.Info("Post:", targetUrl, payload)
 	req, _ := http.NewRequest("POST", targetUrl, strings.NewReader(payload))
 	req.Header.Add("Content-Type", "application/json")
 	response, err := http.DefaultClient.Do(req)
+	if err == nil {
+		logs.Info(response.Body)
+	} else {
+		logs.Info(err.Error())
+	}
 	return response, err
 }
 
 func Delete(targetUrl string, payload string) (*http.Response, error) {
+	logs.Info("Delete:", targetUrl, payload)
 	req, _ := http.NewRequest("DELETE", targetUrl, strings.NewReader(payload))
 	req.Header.Add("Content-Type", "application/json")
 	response, err := http.DefaultClient.Do(req)
+	if err == nil {
+		logs.Info(response.Body)
+	} else {
+		logs.Info(err.Error())
+	}
 	return response, err
 }
 
