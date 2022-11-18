a591b09b74ed935e64c1a3b2435ce32b957c5811
hezhuozhuo <421045249@qq.com>
2022-11-18 09:30:40 +0800

    update history

--- models/ts_kv.go
+++ models/ts_kv.go
@@ -18,17 +18,18 @@ type TSKVResult struct {
 }
 
 type TSKVDblV struct {
-	EntityType string `json:"entity_type" gorm:"primaryKey"`       // 类型：DEVICE
-	EntityID   string `json:"entity_id" gorm:"primaryKey,size:36"` // 设备id
-	Key        string `json:"key" gorm:"primaryKey"`               // 字段
-	TS         int64  `json:"ts" gorm:"primaryKey"`                // 毫秒时间戳
-	BoolV      string `json:"bool_v" gorm:"size:5"`
-	StrV       string `json:"str_v" gorm:"type:longtext"`
-	LongV      int64  `json:"long_v"`
-	DblV       string `json:"dbl_v"`
-	Token      string `json:"token"`
-	Bname      string `json:"bname"`
-	Name       string `json:"name"`
+	EntityType  string `json:"entity_type" gorm:"primaryKey"`       // 类型：DEVICE
+	EntityID    string `json:"entity_id" gorm:"primaryKey,size:36"` // 设备id
+	Key         string `json:"key" gorm:"primaryKey"`               // 字段
+	TS          int64  `json:"ts" gorm:"primaryKey"`                // 毫秒时间戳
+	BoolV       string `json:"bool_v" gorm:"size:5"`
+	StrV        string `json:"str_v" gorm:"type:longtext"`
+	LongV       int64  `json:"long_v"`
+	DblV        string `json:"dbl_v"`
+	Token       string `json:"token"`
+	Bname       string `json:"bname"`
+	Name        string `json:"name"`
+	GatewayName string `json:"gateway_name"`
 }
 
 func (TSKV) TableName() string {
