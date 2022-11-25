1a12f24924abf3bd8606f74ccaad943e8239fac6
hezhuozhuo <421045249@qq.com>
2022-11-25 23:34:30 +0800

    update js script

--- utils/js_script.go
+++ utils/js_script.go
@@ -9,18 +9,30 @@ import (
 func ScriptDeal(code string, msg interface{}, topic string) (string, error) {
 	/*
 		function encodeInp(msg, topic){
-			//编写脚本处理从设备发来消息msg,转为平台可接收的消息规范
-			//将msg转为json对象,如:var jsonObj = JSON.parse(msg);
-			//处理完后将jsonObj转回字符串,如:msg = JSON.stringify(jsonObj);
-			return msg;
+			//将设备自定义msg（自定义形式）数据转换为json形式数据, 设备上报数据到物联网平台时调用
+			//入参：topic string 设备上报消息的 topic
+			//入参：msg byte[] 数组 不能为空
+			//出参：string
+			//处理完后将对象转回字符串形式
+			//例，byte[]转string：var msgString = String.fromCharCode.apply(null, msg);
+			//例，string转jsonObj：msgJson = JSON.parse(msgString);
+			//例，jsonObj转string：msgString = JSON.stringify(msgJson);
+			var msgString = String.fromCharCode.apply(null, msg);
+			return msgString;
 		}
 	*/
 	/*
 		function encodeInp(msg, topic){
-			//编写脚本处理从平台发出的消息msg,转为设备可接收的消息规范
-			//将msg转为json对象,如:var jsonObj = JSON.parse(msg);
-			//处理完后将jsonObj转回字符串,如:msg = JSON.stringify(jsonObj);
-			return msg;
+			//将平台规范的msg（json形式）数据转换为设备自定义形式数据, 物联网平台发送数据数到设备时调用
+			//入参：topic string 设备订阅消息的 topic
+			//入参：msg byte[] 数组 不能为空
+			//出参：string
+			//处理完后将对象转回字符串形式
+			//例，byte[]转string：var msgString = String.fromCharCode.apply(null, msg);
+			//例，string转jsonObj：msgJson = JSON.parse(msgString);
+			//例，jsonObj转string：msgString = JSON.stringify(msgJson);
+			var msgString = String.fromCharCode.apply(null, msg);
+			return msgString;
 		}
 	*/
 	logs.Info("执行脚本")
