86c086c098b3537b9838bf78e724adfb9fd58700
hezhuozhuo <421045249@qq.com>
2022-11-29 21:11:18 +0800

    修改直连设备控制指令bug

--- services/device_service.go
+++ services/device_service.go
@@ -589,14 +589,10 @@ func (*DeviceService) SendMessage(msg []byte, device *models.Device) error {
 		} else { // 协议设备
 			logs.Info("直连协议设备下行脚本处理后：", string(msg))
 			//获取协议插件订阅topic
-			var gatewayDevice *models.Device
-			result := psql.Mydb.Where("id = ?", device.ParentId).First(&gatewayDevice) // 检测网关token是否存在
-			if result.Error == nil {
-				var TpProtocolPluginService TpProtocolPluginService
-				pp := TpProtocolPluginService.GetByProtocolType(device.Protocol, "2")
-				var topic = pp.SubTopicPrefix + gatewayDevice.Token
-				err = cm.SendPlugin(msg, topic)
-			}
+			var TpProtocolPluginService TpProtocolPluginService
+			pp := TpProtocolPluginService.GetByProtocolType(device.Protocol, "2")
+			var topic = pp.SubTopicPrefix + device.Token
+			err = cm.SendPlugin(msg, topic)
 		}
 
 	} else if device.DeviceType == "3" && device.Protocol != "MQTT" { // 协议插件子设备
