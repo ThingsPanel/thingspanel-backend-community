e31c15749553141b99f82ac627a0e2c5d8d471da
hezhuozhuo <421045249@qq.com>
2023-03-28 11:59:36 +0800

    修改激活状态、激活日期字段

--- services/tp_generate_device_service.go
+++ services/tp_generate_device_service.go
@@ -28,19 +28,22 @@ func (*TpGenerateDeviceService) GetTpGenerateDeviceDetail(tp_generate_device_id 
 }
 
 // 获取列表
-func (*TpGenerateDeviceService) GetTpGenerateDeviceList(PaginationValidate valid.TpGenerateDevicePaginationValidate) (bool, []models.TpGenerateDevice, int64) {
-	var TpGenerateDevices []models.TpGenerateDevice
+func (*TpGenerateDeviceService) GetTpGenerateDeviceList(PaginationValidate valid.TpGenerateDevicePaginationValidate) (bool, []valid.TpGenerateDeviceListRsp, int64) {
+	var TpGenerateDevices []valid.TpGenerateDeviceListRsp
 	offset := (PaginationValidate.CurrentPage - 1) * PaginationValidate.PerPage
 	db := psql.Mydb.Model(&models.TpGenerateDevice{})
-	if PaginationValidate.ActivateFlag != "" {
-		db.Where("activate_flag=?", PaginationValidate.ActivateFlag)
+	if PaginationValidate.AddFlag != "" {
+		db.Where("tp_generate_device.add_flag=?", PaginationValidate.AddFlag)
 	}
 	if PaginationValidate.DeviceCode != "" {
-		db.Where("device_code like ?", "%"+PaginationValidate.DeviceCode+"%")
+		db.Where("tp_generate_device.device_code like ?", "%"+PaginationValidate.DeviceCode+"%")
 	}
 	var count int64
 	db.Where("batch_id=?", PaginationValidate.BatchId).Count(&count)
-	result := db.Where("batch_id=?", PaginationValidate.BatchId).Limit(PaginationValidate.PerPage).Offset(offset).Find(&TpGenerateDevices)
+	result := db.Model(&models.TpGenerateDevice{}).
+		Select("tp_generate_device.id,tp_generate_device.add_flag,tp_generate_device.add_date,tp_generate_device.device_code,CASE WHEN ts_kv_latest.key='SYS_ONLINE' THEN '1' ELSE '0' END AS activate_flag").
+		Joins("left join ts_kv_latest on ts_kv_latest.entity_id = tp_generate_device.device_id  and ts_kv_latest.key='SYS_ONLINE' ").
+		Where("tp_generate_device.batch_id=?", PaginationValidate.BatchId).Limit(PaginationValidate.PerPage).Offset(offset).Find(&TpGenerateDevices)
 	if result.Error != nil {
 		logs.Error(result.Error, gorm.ErrRecordNotFound)
 		return false, TpGenerateDevices, 0
@@ -73,7 +76,7 @@ func (*TpGenerateDeviceService) EditTpGenerateDevice(tp_generate_device valid.Tp
 // 删除数据
 func (*TpGenerateDeviceService) DeleteTpGenerateDevice(tp_generate_device models.TpGenerateDevice) error {
 	var activatflag string
-	psql.Mydb.Select("activate_flag").Where("id=?", tp_generate_device.Id).Find(&activatflag)
+	psql.Mydb.Select("add_flag").Where("id=?", tp_generate_device.Id).Find(&activatflag)
 	if activatflag == "1" {
 		return errors.New("已激活不能删除")
 	}
@@ -88,7 +91,7 @@ func (*TpGenerateDeviceService) DeleteTpGenerateDevice(tp_generate_device models
 //生成设备表-批次表-产品表关联查询
 func (*TpGenerateDeviceService) generateDeviceProductBatch(id string) (map[string]interface{}, error) {
 	var gpb map[string]interface{}
-	sql := `select tgd.device_id as device_id,tgd.activate_flag as activate_flag ,tgd.token as token ,tgd.password as password ,tp.protocol_type as protocol_type ,tp.plugin as plugin, 
+	sql := `select tgd.device_id as device_id,tgd.add_flag as add_flag ,tgd.token as token ,tgd.password as password ,tp.protocol_type as protocol_type ,tp.plugin as plugin, 
 	tb.access_address as access_address ,tp.serial_number as serial_number,tb.batch_number as serial_number,tp.device_model_id as device_model_id
 	from tp_generate_device tgd left join tp_batch tb on tgd.batch_id = tb.id left join tp_product tp on  tb.product_id = tp.id where tgd.id = ?`
 	result := psql.Mydb.Raw(sql, id).Scan(&gpb)
@@ -108,8 +111,8 @@ func (*TpGenerateDeviceService) ActivateDevice(generate_device_id string, asset_
 		logs.Error(err.Error())
 		return err
 	}
-	if gpb["activate_flag"] == "1" {
-		return errors.New("设备已激活，不能再次激活！")
+	if gpb["add_flag"] == "1" {
+		return errors.New("设备已添加，不能再次添加！")
 	}
 	var password = ""
 	if gpb["password"] != nil {
@@ -138,8 +141,8 @@ func (*TpGenerateDeviceService) ActivateDevice(generate_device_id string, asset_
 	}
 	//更新激活
 	var tp_generate_device = valid.TpGenerateDeviceValidate{
-		Id:           generate_device_id,
-		ActivateFlag: "1",
+		Id:      generate_device_id,
+		AddFlag: "1",
 	}
 	TpGenerateDeviceService.EditTpGenerateDevice(tp_generate_device)
 	return nil
