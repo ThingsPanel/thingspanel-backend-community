2bb455d32c290c9efe7a600b4bc9efde0d71f8c4
hezhuozhuo <421045249@qq.com>
2023-04-24 21:45:59 +0800

    修复设备日志分页问题

--- services/conditions_log_service.go
+++ services/conditions_log_service.go
@@ -74,7 +74,7 @@ func (*ConditionsLogService) Paginate(conditionsLogListValidate valid.Conditions
 	sqlWhere += where
 	var conditionsLogs []map[string]interface{}
 	var count int64
-	countResult := psql.Mydb.Raw(sqlWhereCount, values...).Count(&count)
+	countResult := psql.Mydb.Raw(sqlWhereCount, values...).Scan(&count)
 	if countResult.Error != nil {
 		//errors.Is(countResult.Error, gorm.ErrRecordNotFound)
 		logs.Error(countResult.Error.Error())
