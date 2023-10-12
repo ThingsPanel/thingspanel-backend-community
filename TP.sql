c73f3bf97a6bd08eaa0b1e565fe0662c5c337889
EnchantedSky <845255519@qq.com>
2023-10-11 21:25:44 -0500

    Update TP.sql

--- TP.sql
+++ TP.sql
@@ -1925,3 +1925,5 @@ ALTER TABLE public.tp_role DROP CONSTRAINT tp_role_un;
 
 -- 产品编号/租户ID 唯一索引
 ALTER TABLE public.tp_product ADD CONSTRAINT tp_product_unique_serial_tenant UNIQUE (serial_number, tenant_id);
+
+ALTER TABLE tp_product ADD COLUMN plugid_id VARCHAR(36) DEFAULT '';
