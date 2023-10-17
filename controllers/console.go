91f4987d716b193d4f7365eca70a34629965cb3c
November-12 <845255519@qq.com>
2023-10-17 09:54:21 +0800

    add

---
+++ controllers/console.go
@@ -0,0 +1,27 @@
+package controllers
+
+import beego "github.com/beego/beego/v2/server/web"
+
+type ConsoleController struct {
+	beego.Controller
+}
+
+func (c *ConsoleController) Add() {
+
+}
+
+func (c *ConsoleController) Edit() {
+
+}
+
+func (c *ConsoleController) Delete() {
+
+}
+
+func (c *ConsoleController) List() {
+
+}
+
+func (c *ConsoleController) Detail() {
+
+}
