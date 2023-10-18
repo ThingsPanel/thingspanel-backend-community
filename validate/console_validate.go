44c58bff456275683cc2b1c15acab0410bdeb1a3
hezhuozhuo <421045249@qq.com>
2023-10-18 15:11:08 +0800

    更新模板接口校验

--- validate/console_validate.go
+++ validate/console_validate.go
@@ -3,17 +3,17 @@ package valid
 type AddConsole struct {
 	// ID             string `json:"id" alias:"ID" valid:"Required; MaxSize(36)"`
 	Name     string `json:"name" alias:"看板名称" valid:"Required; MaxSize(255)"`
-	Data     string `json:"data" alias:"看板数据" valid:"MaxSize(255)"`
-	Config   string `json:"config" alias:"看板配置" valid:"MaxSize(500)"`
-	Template string `json:"templates" alias:"看板模版" valid:"MaxSize(500)"`
+	Data     string `json:"data" alias:"看板数据" valid:"MaxSize(10000)"`
+	Config   string `json:"config" alias:"看板配置" valid:"MaxSize(10000)"`
+	Template string `json:"templates" alias:"看板模版" valid:"MaxSize(10000)"`
 }
 
 type EditConsole struct {
 	ID       string `json:"id" alias:"ID" valid:"Required; MaxSize(36)"`
 	Name     string `json:"name" alias:"看板名称" valid:"MaxSize(255)"`
-	Data     string `json:"data" alias:"看板数据" valid:"MaxSize(255)"`
-	Config   string `json:"config" alias:"看板配置" valid:"MaxSize(500)"`
-	Template string `json:"templates" alias:"看板模版" valid:"MaxSize(500)"`
+	Data     string `json:"data" alias:"看板数据" valid:"MaxSize(10000)"`
+	Config   string `json:"config" alias:"看板配置" valid:"MaxSize(10000)"`
+	Template string `json:"templates" alias:"看板模版" valid:"MaxSize(10000)"`
 }
 
 type DetailAndDetailConsole struct {
