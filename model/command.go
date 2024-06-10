46656cea2608e44b2ab4349ef60b7554b8413c71
hezhuozhuo <421045249@qq.com>
2024-06-10 15:04:46 +0800

    fix bug

--- model/command.go
+++ model/command.go
@@ -1,6 +1,8 @@
 package model
 
 type GetCommandListRes struct {
-	Name       string `json:"data_name"`
-	Identifier string `json:"data_identifier"`
+	Name        string `json:"data_name"`
+	Identifier  string `json:"data_identifier"`
+	Params      string `json:"params"`
+	Description string `json:"description"`
 }
