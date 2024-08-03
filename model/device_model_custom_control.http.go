2a787bc6f67ca8832440c10fa936d2b6e30531af
EnchantedSky <845255519@qq.com>
2024-08-03 15:21:31 +0800

    Update device_model_custom_control.http.go

--- model/device_model_custom_control.http.go
+++ model/device_model_custom_control.http.go
@@ -1,1 +1,22 @@
 package model
+
+type CreateDeviceModelCustomControlReq struct {
+	DeviceTemplateId string  `json:"device_template_id" validate:"required,max=36"` // 设备模版ID
+	Name             string  `json:"name" validate:"required,max=36"`               // 名称
+	ControlType      string  `json:"control_type" validate:"required,max=50"`       // 控制类型
+	Description      *string `json:"description" validate:"omitempty,max=500"`      // 描述
+	Content          *string `json:"content" validate:"omitempty"`                  // 指令内容
+	EnableStatus     string  `json:"enable_status" validate:"required,max=10"`      // 启用状态
+	Remark           *string `json:"remark" validate:"omitempty,max=255"`           // 备注
+}
+
+type UpdateDeviceModelCustomControlReq struct {
+	ID               string  `json:"id" validate:"required,max=36"`                  // ID
+	DeviceTemplateId *string `json:"device_template_id" validate:"omitempty,max=36"` // 设备模版ID
+	Name             *string `json:"name" validate:"omitempty,max=36"`               // 名称
+	ControlType      *string `json:"control_type" validate:"omitempty,max=50"`       // 控制类型
+	Description      *string `json:"description" validate:"omitempty,max=500"`       // 描述
+	Content          *string `json:"content" validate:"omitempty"`                   // 指令内容
+	EnableStatus     *string `json:"enable_status" validate:"omitempty,max=10"`      // 启用状态
+	Remark           *string `json:"remark" validate:"omitempty,max=255"`            // 备注
+}
