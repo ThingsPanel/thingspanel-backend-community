5360190f1983f91984178ee4f4f3d7d14626b156
hezhuozhuo <421045249@qq.com>
2024-09-25 12:44:14 +0800

    add sql

--- sql/4.sql
+++ sql/4.sql
@@ -1,1 +1,2 @@
 ALTER TABLE public.users ADD password_last_updated timestamptz(6) NULL;
+INSERT INTO public.sys_function (id, "name", enable_flag, description, remark) VALUES('function_4', 'shared_account', 'enable', '共享账号', NULL);
