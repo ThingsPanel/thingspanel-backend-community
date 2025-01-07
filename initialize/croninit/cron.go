6843dd51849aabbcb84ec08958efa5766297bcb4
hezhuozhuo <421045249@qq.com>
2025-01-07 16:48:46 +0800

    Fix：定时任务扫描时间修改

--- initialize/croninit/cron.go
+++ initialize/croninit/cron.go
@@ -13,24 +13,22 @@ var (
 
 // 定义任务初始化
 func CronInit() {
-
-	//单次定义成任务
-	c.AddFunc("* * * * * *", func() {
-		logrus.Debug("单次任务开始：")
+	//单次定义成任务 - 每5秒执行一次
+	c.AddFunc("*/5 * * * * *", func() {
+		logrus.Debug("自动化单次任务开始：")
 		service.GroupApp.OnceTaskExecute()
 	})
 
-	//重复定义成任务
-	c.AddFunc("* * * * * *", func() {
-		logrus.Debug("重复定义成任务开始：")
+	//重复定义成任务 - 每5秒执行一次
+	c.AddFunc("*/5 * * * * *", func() {
+		logrus.Debug("自动化重复时间任务开始：")
 		service.GroupApp.PeriodicTaskExecute()
 	})
 
-	// 每天执行数据清理
+	// 每天凌晨2点执行数据清理
 	c.AddFunc("0 2 * * *", func() {
 		logrus.Debug("系统数据清理任务开始：")
 		service.GroupApp.CleanSystemDataByCron()
 	})
-
 	c.Start()
 }
