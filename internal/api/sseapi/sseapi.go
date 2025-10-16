e72b6fdd48f8090955ff6e892eca508aa02c3376
hezhuozhuo <421045249@qq.com>
2025-10-16 14:09:26 +0800

    feat(sseapi): 禁用 nginx 缓冲以防止连接过早关闭

--- internal/api/sseapi/sseapi.go
+++ internal/api/sseapi/sseapi.go
@@ -33,6 +33,7 @@ func (*SSEApi) HandleSystemEvents(c *gin.Context) {
 	c.Writer.Header().Set("Cache-Control", "no-cache")
 	c.Writer.Header().Set("Connection", "keep-alive")
 	c.Writer.Header().Set("Transfer-Encoding", "chunked")
+	c.Writer.Header().Set("X-Accel-Buffering", "no") // 禁用 nginx 缓冲,防止连接过早关闭
 
 	clientID := global.TPSSEManager.AddClient(userClaims.TenantID, userClaims.ID, c.Writer)
 	defer global.TPSSEManager.RemoveClient(userClaims.TenantID, clientID)
