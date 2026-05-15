66794d723519349b398bf17c6610f69bf441fe17
hezhuozhuo <421045249@qq.com>
2026-05-15 15:44:08 +0800

    feat：产品版本信息修改

--- pkg/global/global.go
+++ pkg/global/global.go
@@ -11,7 +11,7 @@ import (
 var (
 	VERSION         = "0.0.17"
 	VERSION_NUMBER  = 17
-	SYSTEM_VERSION  = "v1.1.15"
+	SYSTEM_VERSION  = "v1.2.0"
 	DB              *gorm.DB
 	REDIS           *redis.Client
 	STATUS_REDIS    *redis.Client
