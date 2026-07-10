72954ebf2e040af7bd9ea393da769d12b84c427d
hezhuozhuo <421045249@qq.com>
2026-07-10 17:30:08 +0800

    chore: update system version to v1.2.5

--- pkg/global/global.go
+++ pkg/global/global.go
@@ -11,7 +11,7 @@ import (
 var (
 	VERSION         = "0.0.18"
 	VERSION_NUMBER  = 18
-	SYSTEM_VERSION  = "v1.2.4"
+	SYSTEM_VERSION  = "v1.2.5"
 	DB              *gorm.DB
 	REDIS           *redis.Client
 	STATUS_REDIS    *redis.Client
